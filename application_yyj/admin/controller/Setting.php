b70f20a05f621a1f9610263f49fc0fc2979f6a96
Livenus <42017326+Livenus@users.noreply.github.com>
2018-10-12 19:03:35 +0800

    Add files via upload

--- application_yyj/admin/controller/Setting.php
+++ application_yyj/admin/controller/Setting.php
@@ -1,107 +1,99 @@
-<?php
-namespace app\api\controller;
-use think\Config;
-use think\Response;
-class Setting  extends \app\api\controller\Home {
-        public function _initialize(){
-        $this->Setting = model("Setting");
-        
+<?php 
+namespace app\admin\controller;
+class Setting extends \app\admin\controller\Home{
+    public function _initialize(){
+        parent::_initialize();
+        $this->Setting =model('Setting');
+        $this->MemberGroup =model("MemberGroup");
+
+        $this->lang = \think\Lang::get('log_action');
+        $this->request = \think\Request::instance();
+    }
+    public function indexAc(){
+        $id = $this->Setting->check('setting_index');
+        if($id){
+            $this->admin_priv($id);
+        }else{
+            var_dump('数据库还不完善，稍等');die();
         }
-        public function indexAc(){
-          $this->get_user_id();
-            $data=$this->Setting->select_all();
-            if($data){
-            $data["pic_cash"]=DS."uploads".DS.$data["pic_cash"];
-            $data["pic_income"]=DS."uploads".DS.$data["pic_income"];
-            $data["pic_member"]=DS."uploads".DS.$data["pic_member"];
-            $data["pic_share"]=DS."uploads".DS.$data["pic_share"];
-            $data["pic_share_2"]=DS."uploads".DS.$data["pic_share_2"];
-            $data["pic_wallet"]=DS."uploads".DS.$data["pic_wallet"];
-                return $this->suc($data);
+        $map["stat"]=1;
+         $group=$this->MemberGroup->select_all($map);
+         $set=$this->Setting->get_by_key("member_group");
+         $rate=$this->Setting->select_all();
+         $this->assign("set",$set);
+         $this->assign("rate",$rate);
+         $this->assign("group",$group);
+        return $this->fetch();
+    }
+    public function postionlistAc(){
+        $this->assign('action',$this->request->action());
+        return $this->fetch();
+    }
+    public function editAc(){
+        $id = $this->Setting->check('setting_edit');
+        if($id){
+            $this->admin_priv($id);
+        }else{
+            var_dump('数据库还不完善，稍等');die();
+        }
+        $data=$this->request->param();
+        $req=input("post.");
+        if($this->request->isAjax()){
+            $add["key"]="member_group";
+            $add["value"]=$data["member_group"];
+			if($req){
+            foreach ($req as $k=>$v){
+                $d["key"]=$k;
+                $d["value"]=$v;
+                if($this->Setting->get_by_key($k)){
+                  $status=$this->Setting->editBykey($d,$k,$v);
+                }else{
+               $this->Setting->addItem($d); 
+                }
+
+
+            }
+              admin_log(json_encode($req),"修改表","设置项");
+                return message(1, "修改成功");
+            }else{
+            return message(1, "修改成功");
             }
 
-            return $this->err(900, "没有数据");
-            
         }
-    public  function get_codeAc($phone=""){
-          require('../extend/qrcode/qrlib.php'); 
-          $base="http://".$_SERVER["HTTP_HOST"]."/static/webpage/html/webpage.php?pphone_g=".$phone;
-         //\QRcode::png($base, false, QR_ECLEVEL_L, 4.9, 4.9);
-          
-        $dir='./uploads/qr/'; 
-        if(!is_dir($dir)){
-            mkdir($dir);
-        }
-    $fileName = md5($base).'.png'; 
-    $pngAbsoluteFilePath=$dir.$fileName;
-    if (!file_exists($pngAbsoluteFilePath)) { 
-        $res=\QRcode::png($base, $pngAbsoluteFilePath, QR_ECLEVEL_L, 4.9, 4.9); 
-    } else { 
-    } 
-     echo substr($pngAbsoluteFilePath, 1);
     }
-    //生成二维码
-    public  function get_code_1($phone=""){
-          require('../extend/qrcode/qrlib.php'); 
-          $base="http://".$_SERVER["HTTP_HOST"]."/static/webpage/html/webpage.php?pphone_g=".$phone;
-         //\QRcode::png($base, false, QR_ECLEVEL_L, 4.9, 4.9);
-          
-        $dir='./uploads/qr/'; 
-        if(!is_dir($dir)){
-            mkdir($dir);
-        }
-    $fileName = md5($base. rand(14, 15)).'.png'; 
-    $pngAbsoluteFilePath=$dir.$fileName;
-    if (!file_exists($pngAbsoluteFilePath)) { 
-        $res=\QRcode::png($base, $pngAbsoluteFilePath, QR_ECLEVEL_L, 7.4, 0); 
-    } else { 
-    } 
-     return substr($pngAbsoluteFilePath, 1);
-    }
-    //分享页面
-    public function get_shareAc($phone=""){
-        
-        $url=url("setting/get_code",array("phone"=>$phone));
-        $this->assign("phone",$phone);
-        $str= file_get_contents("./static/webpage/html/code001.html");
-        $pattern = '/<!--替换-->.+?<!--e替换-->/ms';
-        $str=preg_replace($pattern, "", $str);
-        $pattern = '/<!--2替换-->.+?<!--3替换-->/ms';
-        $str=preg_replace($pattern, "", $str);
-        $pattern = '/(<img src=\")(.*?)(\".*?>)/i';
-        $replacement = '${1}'.$url.'$3';
-       echo preg_replace($pattern, $replacement, $str);
-
-    }
-    public  function get_downAc($phone=""){
-          require('../extend/qrcode/qrlib.php'); 
-          $url=$this->Setting->get_by_key("android_url");
-         \QRcode::png($url, false, QR_ECLEVEL_L, 4.9, 0);
-         
-        
-    }
-    public function get_img_sAc($phone=""){
-        $dir='./uploads/qr/'; 
-        if(!is_dir($dir)){
-            mkdir($dir);
-        }
-        $img=$this->get_code_1($phone);
+    public  function set_codeAc(){
         $imgpos=$this->Setting->get_by_key("imgpos");
         $imgpos_d= explode(",", $imgpos["value"]);
-        $pos=array($imgpos_d[0],$imgpos_d[1]);
-        $back='.'.$imgpos_d[2];
-        $image = \think\Image::open($back);
-        // 给原图左上角添加水印并保存water_image.png
-        $out=$dir.md5($phone.$imgpos['value'].rand(14, 15)).".png";
-        if(file_exists($out)){
-            $img=substr($out,1);
-            $img=$img."?t=".time();
-        }else{
-        $up='.'.$img;
-        $res=$image->water($up,$pos)->save($out);  
-        $img=substr($out,1);
-        $img=$img."?t=".time();
-        }
-        return $this->suc($img);
+        $this->assign("imgpos_d",$imgpos_d[2]);
+        $this->assign("phone",1);
+        
+        return view("./static/webpage/html/code001.html");
+        
     }
+    public  function saveAc(){ 
+        $str=input("post.save");
+        $res=file_put_contents("./static/webpage/html/code001.html", $str);
+        return message(1,"保存成功");
+        
+    }
+    public function save_img_posAc($x="",$y="",$imgurl=""){
+        if($x&&$y&&$imgurl){
+        }else{
+            echo "坐标，图片地址必填";
+        }
+        $data["value"]="$x,$y,$imgurl";
+        $data["key"]="imgpos";
+        if($this->Setting->get_by_key("imgpos")){
+           $status=$this->Setting->editBykey($data, "imgpos",$data["value"]);
+        }else{
+            $status=$this->Setting->addItem($data); 
+        }
+        if($status['stat']==1){
+            
+            return message(1, "修改成功");
+        }
+        return message(0, "修改失败");
+    }
+
 }
+?>
